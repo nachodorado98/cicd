d31c95eae7809715e940d6f79a8df400e3d80431
nacho dorado <natxo98@gmail.com>
2025-07-20 17:37:00 +0200

    Test creado para el endpoint health

--- python/tests/testapp/test_app.py
+++ python/tests/testapp/test_app.py
@@ -6,3 +6,12 @@ def test_pagina_inicial(cliente):
 
 	respuesta.status_code==200
 	assert "Tu Nachitoo te quiere mucho jejeje" in contenido
+
+def test_pagina_health(cliente):
+
+	respuesta=cliente.get("/health")
+
+	contenido=respuesta.data.decode()
+
+	respuesta.status_code==200
+	assert "OK" in contenido
