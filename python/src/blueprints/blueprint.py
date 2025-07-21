c32565d4e6d4d7528daea1419a75a0e067d25d11
nacho dorado <natxo98@gmail.com>
2025-07-21 13:11:12 +0200

    Nuevo endpoint en la app

--- python/src/blueprints/blueprint.py
+++ python/src/blueprints/blueprint.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 bp=Blueprint("blueprint", __name__)
 
 # Vista de la pagina principal
-@bp.route("/", methods=["GET"])
+@bp.route("/")
 def inicio()->str:
 
 	return f"<h1>Mi Amanda, mi amor</h1>"
@@ -12,3 +12,8 @@ def inicio()->str:
 def health():
 	
     return "OK", 200
+
+@bp.route("/golden")
+def secret()->str:
+
+	return f"<h1>Hola Golden</h1>"
